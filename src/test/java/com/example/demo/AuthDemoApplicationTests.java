4db7e450c1f4ea5166fa9617dd5564dbdbfbde43
Dave Syer <dsyer@vmware.com>
2024-06-04 08:39:53 +0100

    Collect more logs on startup

--- src/test/java/com/example/demo/AuthDemoApplicationTests.java
+++ src/test/java/com/example/demo/AuthDemoApplicationTests.java
@@ -14,6 +14,7 @@ class AuthDemoApplicationTests {
 	public static void main(String[] args) {
 		System.setProperty("spring.main.cloud-platform", "kubernetes");
 		// System.setProperty("kubernetes.service.host", "localhost:43319");
+		// System.setProperty("debug", "true");
 		System.setProperty("management.kubernetes.skip-ssl-validation", "true");
 		SpringApplication.run(AuthDemoApplication.class);
 	}
